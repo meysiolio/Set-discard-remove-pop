a7a6f1df6881fafe293e84e322bf9a450b7beea4
Meysiolio <saturninsky@gmail.com>
2021-10-05 12:59:35 +0330

    first commit

---
+++ Set discard-remove-pop.py
@@ -0,0 +1,16 @@
+n = int(input())
+s = set(map(int, input().split()))
+
+commNum = int(input())
+
+for i in range(commNum):
+    command = input()
+    
+    if command.split()[0] == 'pop':
+        s.pop()
+    if command.split()[0] == 'remove':
+        s.remove(int(command.split()[1]))
+    if command.split()[0] == 'discard':
+        s.discard(int(command.split()[1]))
+        
+print(sum(s))
